92192910d2928ddff64453ef1b6b751c7b3f30f9
Criste Phillips <criste.phillips@yahoo.com>
2022-04-02 15:10:09 -0400

    manifest and service-worker initial

---
+++ public/service-worker.js
@@ -0,0 +1,19 @@
+const APP_PREFIX = 'BudgetTracker-';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+const FILES_TO_CACHE = [
+    "./index.html",
+    "./manifest.json",
+    "./icons/icon_512x512.png",
+    "./icons/icon_384x384.png",
+    "./icons/icon_192x192.png",
+    "./icons/icon_152x152.png",
+    "./icons/icon_144x144.png",
+    "./icons/icon_128x128.png",
+    "./icons/icon_96x96.png",
+    "./icons/icon_72x72.png",
+];
+
+
+
