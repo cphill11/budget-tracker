91f959afb897cd51717c613f3c484122398a563f
Criste Phillips <criste.phillips@yahoo.com>
2022-04-02 15:13:30 -0400

    event listeners added to service-workers

--- public/service-worker.js
+++ public/service-worker.js
@@ -15,5 +15,61 @@ const FILES_TO_CACHE = [
     "./icons/icon_72x72.png",
 ];
 
-
+// 'self' instantiates listeners on server worker
+self.addEventListener("install", function (e) {
+    // app waits until work is complete to terminate service worker
+    e.waitUntil(
+      // find specific cache by name
+      caches.open(CACHE_NAME).then(function (cache) {
+        console.log("installing cache : " + CACHE_NAME);
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );
+    self.skipWaiting();
+  });
+  
+  // event listener to activate event
+  self.addEventListener("activate", function (e) {
+    e.waitUntil(
+      caches.keys().then(function (keyList) {
+        // `keyList` contains all cache names under a username.github.io; filters out those w/ this app prefix to create keeplist
+        let cacheKeeplist = keyList.filter(function (key) {
+          return key.indexOf(APP_PREFIX);
+        });
+        // add current cache name to keeplist
+        cacheKeeplist.push(CACHE_NAME);
+  
+        return Promise.all(
+          keyList.map(function (key, i) {
+            if (cacheKeeplist.indexOf(key) === -1) {
+              console.log("deleting cache : " + keyList[i]);
+              return caches.delete(keyList[i]);
+            }
+          })
+        );
+      })
+    );
+    self.clients.claim();
+  });
+  
+  // listen for response of cached resources
+  self.addEventListener("fetch", function (e) {
+    // listen for event, fetch event, log URL of requested resource
+    console.log("fetch request : " + e.request.url);
+    e.respondWith(
+      // if resource matches one already saved in cache, URL will be logged
+      caches.match(e.request).then(function (request) {
+        if (request) {
+          // if cache is available, respond with cache
+          console.log("responding with cache : " + e.request.url);
+          return request;
+        } else {
+          // if there are no cache, try fetching request from online network
+          console.log("file is not cached, fetching : " + e.request.url);
+          return fetch(e.request);
+        }
+      })
+    );
+  });
+  
 
